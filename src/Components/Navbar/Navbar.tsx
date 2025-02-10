288e21bae5b9dcd08100dc5452f8e803aeec9f1a
CHINTHOJU SHIVAKUMAR <shivakumarchinthoju@gmail.com>
2025-02-10 13:40:21 +0530

    final

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
+import { Box, Modal } from "@mui/material";
+// import Resume from "../../../public/ShivaResume02062025.pdf"
 
 interface NavItems {
   menu: string;
   url: string;
 }
 
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: { xs: "90%", sm: "80%", md: "60%", lg: "60%", xl: "60%", xxl: "80%" },
+  height: "80%",
+  bgcolor: "background.paper",
+  // border: "1px solid white",
+  boxShadow: 24,
+  p: 4,
+};
+
 const Navbar: React.FC = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   const navItems: NavItems[] = [
     {
       menu: "Home",
@@ -21,6 +37,10 @@ const Navbar: React.FC = () => {
       url: "projects",
     },
     {
+      menu: "Resume",
+      url: "resume",
+    },
+    {
       menu: "Contact",
       url: "contact",
     },
@@ -28,6 +48,9 @@ const Navbar: React.FC = () => {
 
   const handleScroll = (id: string) => {
     const section = document.getElementById(id);
+    if (id === "resume") {
+      setIsOpen(true);
+    }
     if (section) {
       section.scrollIntoView({ behavior: "smooth" });
     }
@@ -52,6 +75,36 @@ const Navbar: React.FC = () => {
           })}
         </div>
       </nav>
+      <Modal open={isOpen}>
+        <Box sx={style}>
+          <div className="modal">
+            <div className="close" onClick={() => setIsOpen(false)}>
+              <p>X</p>
+            </div>
+            <div className="resume-containter">
+              <iframe
+                src="ShivaResume02062025.pdf"
+                className="resume"
+                width="100%"
+                height="400px"
+              ></iframe>
+            </div>
+            <div className="button-container">
+              <button className="resume-download" type="button">
+                <a
+                  href={"ShivaResume02062025.pdf"}
+                  download={"ShivaResume02062025.pdf"}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  // style={{ textDecoration: "none", color:"white" }}
+                >
+                  Download
+                </a>
+              </button>
+            </div>
+          </div>
+        </Box>
+      </Modal>
     </div>
   );
 };
